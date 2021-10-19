5f89547d807f3643dc68b7470749bffcda7b6271
Atul Kumar Rai <atulkumr@andrew.cmu.edu>
2021-10-19 14:00:05 -0400

    Initial Commit

---
+++ TestProjectAPIFireBase/TestProjectAPIFireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectAPIFireBaseApp.swift
+//  TestProjectAPIFireBase
+//
+//  Created by Atul Kumar Rai on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectAPIFireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
