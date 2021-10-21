f919a73b13616379c8c0258921dadde3c634da4e
Atul Kumar Rai <atulkumr@andrew.cmu.edu>
2021-10-20 23:58:22 -0400

    Check-in FireStore Integration files

--- TestProjectAPIFireBase/TestProjectAPIFireBaseApp.swift
+++ TestProjectAPIFireBase/TestProjectAPIFireBaseApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TestProjectAPIFireBaseApp: App {
+  init() {
+    FirebaseApp.configure()
+  }
     var body: some Scene {
         WindowGroup {
             ContentView()
